fc84d3135e520221aa27684e91936adecd730772
Your Name <you@example.com>
2025-07-08 19:51:33 +0500

    feature branch add refresh-interval in main.py

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,9 @@ def get_db():
         yield db
     finally:
         db.close()
-
+@app.get("/refresh-interval")
+def get_interval():
+    return {"interval": 10}
 @app.get("/hosts", response_model=List[Host])
 def get_hosts(db: Session = Depends(get_db)):
     return db.query(HostDB).all()
